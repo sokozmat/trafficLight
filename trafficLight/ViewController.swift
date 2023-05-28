3ef8d16920c7d5728b13478ceb4573d739d16fa9
Sokozmat <smerekabeats@gmail.com>
2023-05-28 17:29:20 +0200

    lights work

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var trafficLightButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLightButton.layer.cornerRadius = 10
+        redLight.alpha = 0.3
+        redLight.layer.cornerRadius = 55
+        yellowLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 55
+        greenLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 55
     }
-
-
+    
+    @IBAction func buttonPushed() {
+        trafficLightButton.setTitle("NEXT", for: .normal)
+        if redLight.alpha != 1 && yellowLight.alpha != 1 {
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+        } else if redLight.alpha == 1 {
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+        } else {
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            redLight.alpha = 0.3
+        }
+    }
 }
-
